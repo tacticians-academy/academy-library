cf19f732c28fe60bbc58bdbb45e8c087cba082a7
Kyle <kylecoburn@gmail.com>
2022-03-03 15:40:43 -0700

    🏗 assume raw icon data

--- dist/assets.ts
+++ dist/assets.ts
@@ -1,6 +1,6 @@
 export const ASSET_PREFIX = 'https://raw.communitydragon.org/pbe/game/'
 
-export function getIconURL(deriveAsset: {icon: string | null | undefined}, isRaw: boolean) {
+export function getIconURL(deriveAsset: {icon: string | null | undefined}, isRaw: boolean = true) {
 	if (deriveAsset.icon == null) {
 		console.warn('getIconURL', deriveAsset)
 		return ''
