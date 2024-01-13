80556d284aae5a7782f2d526075359c775304d7d
Kyle <kylecoburn@gmail.com>
2024-01-13 14:57:43 -0800

    🔖 0.32.0

--- build/update-current.ts
+++ build/update-current.ts
@@ -3,6 +3,6 @@ import { SET_DATA } from '../dist/index.js'
 import { updateSet } from './etl/index.js'
 import { getLatestSetNumberFrom } from './etl/extract.js'
 
-// const currentSetNumber = 3 //SAMPLE
+// const currentSetNumber = 3.5 //SAMPLE
 const currentSetNumber = getLatestSetNumberFrom(SET_DATA)
 await updateSet(currentSetNumber)
